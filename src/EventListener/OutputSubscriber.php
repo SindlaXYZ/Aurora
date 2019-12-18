231e3cc1be5c7cf53100d141c8a9f6fe1468ae0a
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2019-12-18 09:36:41 +0200

    * OutputSubscriber

--- src/EventListener/OutputSubscriber.php
+++ src/EventListener/OutputSubscriber.php
@@ -39,12 +39,12 @@ class OutputSubscriber implements EventSubscriberInterface
     public function onKernelResponse(ResponseEvent $event)
     {
         $pathInfo = $event->getRequest()->getPathInfo();
-        if (true == $this->container->getParameter('aurora.minify.output') && array_map(function ($extension) use ($pathInfo) {
+        if (true == $this->container->getParameter('aurora.minify.output') && 0 == count(array_filter($this->container->getParameter('aurora.minify.output.ignore.extensions'), function ($extension) use ($pathInfo) {
                 // If extensions found in path info
                 if (substr_compare($pathInfo, $extension, strlen($pathInfo) - strlen($extension), strlen($extension)) === 0) {
-                    return false;
+                    return $extension;
                 }
-            }, $this->container->getParameter('aurora.minify.output.ignore.extensions'))) {
+            }))) {
 
             $response = $event->getResponse();
             if (!$response->headers->get('X-Do-Not-Minify') && !$response->headers->get('x-do-not-minify') && !method_exists($response, 'getFile') && !in_array($response->headers->get('content-type'), $this->container->getParameter('aurora.minify.output.ignore.content.type'))) {
