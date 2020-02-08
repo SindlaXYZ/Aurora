5f2f9d171b1d830d479c308d415218edc5d04f4d
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2020-02-08 15:53:56 +0200

    + OutputSubscriber tests

---
+++ tests/EventListener/OutputSubscriberTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Tests\EventListener;
+
+// PHPUnit
+use PHPUnit\Framework\TestCase;
+
+// Symfony
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+// Sindla
+use Sindla\Bundle\AuroraBundle\EventListener\OutputSubscriber;
+
+/**
+ * clear; php phpunit.phar -c phpunit.xml.dist vendor/sindla/aurora/tests/EventListener/OutputSubscriberTest.php --no-coverage
+ */
+class OutputSubscriberTest extends KernelTestCase
+{
+    private $kernelTest;
+    private $containerTest;
+
+    protected function setUp(): void
+    {
+        $this->kernelTest    = self::bootKernel();
+        $this->containerTest = $this->kernelTest->getContainer();
+    }
+
+    public function testFake()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+    }
+
+    public function testXRobotsTag()
+    {
+        $this->assertFalse(preg_match(OutputSubscriber::PREG_DEV_PREFIX, 'stagingoind.com'));
+    }
+}
