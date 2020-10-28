ab945f9b9ad77be028aa13b122bc406ff57ad570
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2020-10-28 18:14:35 +0200

    ^ Twig extension: + compressCSSJS()

--- src/Utils/Twig/UtilityExtension.php
+++ src/Utils/Twig/UtilityExtension.php
@@ -3,9 +3,12 @@
 namespace Sindla\Bundle\AuroraBundle\Utils\Twig;
 
 // Symfony
+use Sindla\Bundle\AuroraBundle\Utils\Git\Git;
+use Sindla\Bundle\AuroraBundle\Utils\Sanitizer\Sanitizer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Yaml\Yaml;
 
 // Twig
 use Twig\Extension\AbstractExtension;
@@ -18,7 +21,6 @@ use MatthiasMullie\Minify;
 
 // Sindla
 use Sindla\Bundle\AuroraBundle\Utils\PWA\PWA;
-use Symfony\Component\Yaml\Yaml;
 
 class UtilityExtension extends AbstractExtension
 {
@@ -102,6 +104,7 @@ class UtilityExtension extends AbstractExtension
             new TwigFunction('ip2City', [$this, 'ip2City']),
             new TwigFunction('compressCss', [$this, 'compressCss']),
             new TwigFunction('compressJs', [$this, 'compressJs']),
+            new TwigFunction('compressCSSJS', [$this, 'compressCSSJS']),
 
             new TwigFunction('manifest', [$this, 'manifest']),
             new TwigFunction('pwa', [$this, 'pwa']),
@@ -149,7 +152,7 @@ class UtilityExtension extends AbstractExtension
         echo $html;
     }
 
-    public function getBuild($limit = NULL)
+    public function getBuild($limit = null)
     {
         $serviceGit = $this->container->get('aurora.git');
         $build      = $serviceGit->getHash();
@@ -207,9 +210,10 @@ class UtilityExtension extends AbstractExtension
      * @param       $Request
      * @param mixed ...$assets
      */
-    public function compressCss(Request $Request, $combine, $minify, ...$assets)
+    public function compressCssV1(Request $Request, $combine, $minify, ...$assets)
     {
         $serviceGit = $this->container->get('aurora.git');
+        $root       = $this->container->getParameter('root');
 
         if (!$combine) {
             foreach ($assets as $asset) {
@@ -227,7 +231,7 @@ class UtilityExtension extends AbstractExtension
             // Combine multiple assets into one single file
         } else {
             $root          = $this->container->getParameter('aurora.root');
-            $cacheFileName = $serviceGit->getHash() . '.css';
+            $cacheFileName = sha1(json_encode($assets) . $serviceGit->getHash()) . '.css';
             $cacheFilePath = "{$root}/web/static/compiled/{$cacheFileName}";
 
             /**
@@ -237,6 +241,15 @@ class UtilityExtension extends AbstractExtension
                 unlink($cacheFilePath);
             }
 
+            // Remove comments
+            $regex = [
+                "`^([\t\s]+)`ism"                       => '',
+                "`^\/\*(.+?)\*\/`ism"                   => "",
+                "`(\A|[\n;]+)/\*.+?\*/`s"               => "$1",
+                "`(\A|[;\s]+)//.+\R`"                   => "$1\n",
+                "`(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+`ism" => "\n"
+            ];
+
             // If cache file does't exists
             if (!file_exists($cacheFilePath)) {
                 $outputHeader = '';
@@ -245,12 +258,23 @@ class UtilityExtension extends AbstractExtension
                 foreach ($assets as $asset) {
                     $asset = trim($asset);
                     if (strlen($asset) > 3) {
-                        $css = '';
+                        $basename = basename($asset);
+                        $baseDir  = str_ireplace($basename, '', $asset);
+                        $css      = '';
                         if (preg_match('/http:|https:/', $asset)) {
                             $outputHeader .= ';@import url("' . $asset . '");';
                         } else {
-                            $css = ';' . file_get_contents($root . '/public/' . $asset);
+                            $css = "/*! {$basename} ^ {$asset} ^ {$baseDir} */" . file_get_contents($root . '/public/' . $asset);
+
+                            preg_match_all("/url\((?!['\"]?(?:data|http):)['\"]?([^'\"\)]*)['\"]?\)/", $css, $matches);
+                            foreach ($matches[0] as $urlToImport) {
+                                $urlToImport2 = str_ireplace('url(', "url({$baseDir}", $urlToImport);
+                                $css          = str_replace($urlToImport, $urlToImport2, $css);
+                            }
                         }
+
+                        // Remove comments
+                        $css = preg_replace(array_keys($regex), $regex, $css);
 
                         if ($minify) {
                             $minifier->add($css);
@@ -262,16 +286,58 @@ class UtilityExtension extends AbstractExtension
 
                 $output = $outputHeader . $output;
 
-                $baseurl = $Request->getScheme() . '://' . $Request->getHttpHost() . $Request->getBasePath();
-
+                //$baseurl = $Request->getScheme() . '://' . $Request->getHttpHost() . $Request->getBasePath();
                 //$output = str_replace('../webfonts/', "{$baseurl}/static/fontawesome-free/5.4.2/webfonts/", $output);
 
                 $cacheContent = ($minify ? $minifier->minify() : $output);
-                $cacheContent = str_replace('url(/static/', 'url(' . $baseurl . '/static/', $cacheContent);
+
+                //$cacheContent = str_replace('url(/static/', 'url(' . $baseurl . '/static/', $cacheContent);
                 file_put_contents("{$root}/public/static/compiled/{$cacheFileName}", $cacheContent);
             }
 
             echo '<link type="text/css" rel="stylesheet" href="/static/compiled/' . $cacheFileName . '" />';
+        }
+    }
+
+    /**
+     * Minify the CSS into one single file
+     * Save (if not exists) the minified file using the last GIT branch hash tag
+     *
+     * https://github.com/matthiasmullie/minify
+     *
+     * @param       $Request
+     * @param mixed ...$assets
+     */
+    public function compressCss(Request $Request, $combine, $minify, ...$assets)
+    {
+        $serviceGit = $this->container->get('aurora.git');
+
+        if (!$combine && !$minify) {
+            foreach ($assets as $asset) {
+                $asset = trim($asset);
+                if (strlen($asset) > 3) {
+                    if (!preg_match('/http:|https:/', $asset)) {
+                        $asset = $asset . '?v=' . (('dev' === $this->container->getParameter('kernel.environment')) ? uniqid() : $serviceGit->getHash());
+                    }
+                    echo "\n\t" . '<link type="text/css" rel="stylesheet" href="' . $asset . '" />';
+                }
+            }
+        } else {
+            $auroraRootDir = $this->container->getParameter('aurora.root'); // %kernel.project_dir%
+            $auroraTmpDir  = $this->container->getParameter('aurora.tmp');  // %kernel.project_dir%/var/tmp
+
+            // '%kernel.project_dir%/public/static
+            // $auroraStaticDir = $this->container->getParameter('aurora.static');
+
+            // Can be: %kernel.project_dir%/var/tmp/compiled
+            $staticServerDir = 0 ? preg_replace('~//+~', '/', ($auroraTmpDir . '/compiled')) : preg_replace('~//+~', '/', ($auroraRootDir . '/public/static/compiled'));
+            $staticWebDir    = 0 ? '/aurora/compiled' : '/static/compiled';
+
+            if ('dev' === $this->container->getParameter('kernel.environment') && !is_dir($staticServerDir) && !mkdir($staticServerDir, 0777, true)) {
+                throw new \RuntimeException("[AURORA] Cannot create cache dir `{$staticServerDir}`");
+            }
+
+            $combined = '/*' . date('Y-m-d H:i:s') . '*/';
         }
     }
 
@@ -289,15 +355,19 @@ class UtilityExtension extends AbstractExtension
     {
         // TODO: external JS files : preg_match('/http:|https:|ftp:/', $asset)
 
+        $serviceGit = $this->container->get('aurora.git');
+
         if (!$combine && !$minify) {
             foreach ($assets as $asset) {
                 $asset = trim($asset);
                 if (strlen($asset) > 3) {
+                    if (!preg_match('/http:|https:/', $asset)) {
+                        $asset = $asset . '?v=' . (('dev' === $this->container->getParameter('kernel.environment')) ? uniqid() : $serviceGit->getHash());
+                    }
                     echo "\n\t" . '<script src="' . $asset . '"></script>';
                 }
             }
         } else {
-            $serviceGit    = $this->container->get('aurora.git');
             $auroraRootDir = $this->container->getParameter('aurora.root'); // %kernel.project_dir%
             $auroraTmpDir  = $this->container->getParameter('aurora.tmp');  // %kernel.project_dir%/var/tmp
 
@@ -308,7 +378,7 @@ class UtilityExtension extends AbstractExtension
             $staticServerDir = 0 ? preg_replace('~//+~', '/', ($auroraTmpDir . '/compiled')) : preg_replace('~//+~', '/', ($auroraRootDir . '/public/static/compiled'));
             $staticWebDir    = 0 ? '/aurora/compiled' : '/static/compiled';
 
-            if ('dev' === $this->container->getParameter('kernel.environment') && !is_dir($staticServerDir) && !mkdir($staticServerDir, 0777, TRUE)) {
+            if ('dev' === $this->container->getParameter('kernel.environment') && !is_dir($staticServerDir) && !mkdir($staticServerDir, 0777, true)) {
                 throw new \RuntimeException("[AURORA] Cannot create cache dir `{$staticServerDir}`");
             }
 
@@ -319,10 +389,10 @@ class UtilityExtension extends AbstractExtension
                     $assetContent = file_get_contents($auroraRootDir . '/public/' . $asset);
 
                     if ($combine && $minify) {
-                        $combined .= \JShrink\Minifier::minify($assetContent, ['flaggedComments' => FALSE]) . ';';
+                        $combined .= \JShrink\Minifier::minify($assetContent, ['flaggedComments' => false]) . ';';
                     } else {
                         $sha1         = sha1($asset . $serviceGit->getHash()) . '.js';
-                        $minifiedCode = \JShrink\Minifier::minify($assetContent, ['flaggedComments' => FALSE]);
+                        $minifiedCode = \JShrink\Minifier::minify($assetContent, ['flaggedComments' => false]);
                         if ('dev' === $this->container->getParameter('kernel.environment') || !file_exists("{$staticServerDir}/{$sha1}")) {
                             file_put_contents("{$staticServerDir}/{$sha1}", '/*' . date('Y-m-d H:i:s') . '*/' . $minifiedCode);
                         }
@@ -337,6 +407,98 @@ class UtilityExtension extends AbstractExtension
                     file_put_contents("{$staticServerDir}/{$sha1}", $combined);
                 }
                 echo "\n\t" . '<script src="' . $staticWebDir . '/' . $sha1 . '"></script>';
+            }
+        }
+    }
+
+    /**
+     * Minify CSS|JS into one single file
+     *
+     * https://github.com/matthiasmullie/minify
+     * https://github.com/tedious/JShrink
+     *
+     * @param Request $Request
+     * @param string  $assetType
+     * @param bool    $combineAndMinify
+     * @param mixed   ...$assets
+     * @throws \Exception
+     */
+    public function compressCSSJS(Request $Request, string $assetType, bool $combineAndMinify = false, ...$assets)
+    {
+        $auroraRootDir = $this->container->getParameter('aurora.root'); // %kernel.project_dir%
+
+        /** @var Sanitizer $serviceSanitizer */
+        $serviceSanitizer = $this->container->get('aurora.sanitizer');
+
+        /** @var Git $serviceGit */
+        $serviceGit = $this->container->get('aurora.git');
+
+        // Can be: %kernel.project_dir%/var/tmp/compiled
+        # $auroraTmpDir  = $this->container->getParameter('aurora.tmp');  // %kernel.project_dir%/var/tmp
+        # $staticServerDir = (0 ? preg_replace('~//+~', '/', ($auroraTmpDir . '/compiled')) : preg_replace('~//+~', '/', ($auroraRootDir . '/public/static/compiled')));
+        # $staticWebDir    = (0 ? '/aurora/compiled' : '/static/compiled');
+
+        $onDev = boolval('dev' === $this->container->getParameter('kernel.environment'));
+
+        if ($combineAndMinify) {
+            $combineAndMinifyOutputContentHead = '';
+            $combineAndMinifyOutputContent     = '';
+            $combineAndMinifyOutputFileName    = (sha1(json_encode($assets) . $serviceGit->getHash()) . ('css' == $assetType ? '.css' : '.js'));
+            $combineAndMinifyOutputAbsPath     = "{$auroraRootDir}/public/static/compiled/{$combineAndMinifyOutputFileName}";
+            $combineAndMinifyOutputWebPath     = "/static/compiled/{$combineAndMinifyOutputFileName}";
+        }
+
+        foreach ($assets as $index => $assetWebPath) {
+            if (strlen($assetWebPath) > 3) {
+
+                $assetWebPath  = trim($assetWebPath);                             // relative to domain root, eg: static/css/main.css or external file
+                $assetAbsPath  = "{$auroraRootDir}/public/{$assetWebPath}";       // relative to server dirs, eg: /srv/domain.tld/public/static/css/main.css
+                $assetBasename = basename($assetWebPath);                         // main.css
+                $assetBaseDir  = str_ireplace($assetBasename, '', $assetWebPath); // static/css/
+
+                // No combine in one single file, and do not minify
+                if (!$combineAndMinify) {
+                    if (!preg_match('/http:|https:|ftp:/', $assetWebPath)) {
+                        $assetWebPath = ($assetWebPath . '?v=' . ($onDev ? uniqid() : $serviceGit->getHash()));
+                    }
+
+                    if ('css' == $assetType) {
+                        echo "\n\t" . '<link type="text/css" rel="stylesheet" href="' . $assetWebPath . '" />';
+                    }
+
+                    if ('js' == $assetType) {
+                        echo "\n\t" . '<script src="' . $assetWebPath . '"></script>';
+                    }
+                }
+
+                if ($combineAndMinify && ($onDev || !file_exists($combineAndMinifyOutputAbsPath))) {
+                    if (!preg_match('/http:|https:|ftp:/', $assetWebPath)) {
+                        $combineAndMinifyOutputContent .= ('css' == $assetType
+                            ? $serviceSanitizer->cssMinify(file_get_contents($assetAbsPath), $assetWebPath)
+                            : \JShrink\Minifier::minify(file_get_contents($assetAbsPath), ['flaggedComments' => false]) . ';'
+                        );
+                    } else {
+                        if ('css' == $assetType) {
+                            $combineAndMinifyOutputContentHead .= ';@import url("' . $assetWebPath . '");';
+                        } else {
+                            trigger_error('External JS files not supported.');
+                        }
+                    }
+                }
+            }
+        }
+
+        if ($combineAndMinify) {
+            if ($onDev || !file_exists($combineAndMinifyOutputAbsPath)) {
+                file_put_contents($combineAndMinifyOutputAbsPath, '/*' . date('Y-m-d H:i:s') . '*/' . ($combineAndMinifyOutputContentHead . $combineAndMinifyOutputContent));
+            }
+
+            if ('css' == $assetType) {
+                echo "\n\t" . '<link type="text/css" rel="stylesheet" href="' . $combineAndMinifyOutputWebPath . '" />';
+            }
+
+            if ('js' == $assetType) {
+                echo "\n\t" . '<script src="' . $combineAndMinifyOutputWebPath . '"></script>';
             }
         }
     }
