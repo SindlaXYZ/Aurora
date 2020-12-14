25018cfc41fdf0e70dad035caf94205d74168cbf
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2020-12-14 20:41:38 +0200

    ^

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,11 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+if(is_file(dirname(__DIR__).'/vendor/autoload.php')) {
+    require dirname(__DIR__) . '/vendor/autoload.php';
+} else {
+    require __DIR__.'/../vendor/autoload.php';
+}
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
