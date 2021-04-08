1dd5e9e7da88a2f6971ff741105b2df9c404f554
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2021-04-08 18:34:08 +0300

    + /tests/Command/

---
+++ tests/Command/ComposerCommandTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Tests\Command;
+
+// PHPUnit
+use PHPUnit\Framework\TestCase;
+
+// Symfony
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+// Sindla
+use Sindla\Bundle\AuroraBundle\EventListener\OutputSubscriber;
+
+/**
+ * clear; php phpunit.phar -c phpunit.xml.dist vendor/sindla/aurora/tests/Command/ComposerCommandTest.php --no-coverage
+ */
+class ComposerCommandTest extends KernelTestCase
+{
+    private $kernelTest;
+    private $containerTest;
+
+    protected function setUp(): void
+    {
+        $this->kernelTest    = self::bootKernel();
+        $this->containerTest = $this->kernelTest->getContainer();
+    }
+
+    public function testFake()
+    {
+        $this->assertTrue(true);
+        $this->assertFalse(false);
+    }
+}
