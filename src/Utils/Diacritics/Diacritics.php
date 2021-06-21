5739a074715fdf0463bd7c7095e9d64f4cb852dc
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2021-06-21 19:57:10 +0300

    ^

--- src/Utils/Diacritics/Diacritics.php
+++ src/Utils/Diacritics/Diacritics.php
@@ -9,19 +9,19 @@ use Sindla\Bundle\AuroraBundle\Utils\Diacritics\Extension\Romanian;
  */
 class Diacritics
 {
-    private $sets = [];
+    private array $sets = [];
 
     public function useRomanian()
     {
         $this->sets = Romanian::sets();
     }
 
-    public function inject(array $collection)
+    public function inject(array $collection): void
     {
         $this->sets = array_merge($this->sets, $collection);
     }
 
-    public function modify(string $string)
+    public function modify(string $string): string
     {
         $string = str_replace(['Ş', 'ş', 'Ţ', 'ţ'], ['Ș', 'ș', 'Ț', 'ț'], $string);
 
