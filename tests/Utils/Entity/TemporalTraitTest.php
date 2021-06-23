9ea80a783d95a0690436874a909287e31edc2490
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2021-06-23 17:23:27 +0300

    ^

--- tests/Utils/Entity/TemporalTraitTest.php
+++ tests/Utils/Entity/TemporalTraitTest.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace Sindla\Bundle\AuroraBundle\Tests\Utils\Entity;
 
+// Core
+use DateTime;
+use InvalidArgumentException;
+
 // PHPUnit
 use PHPUnit\Framework\TestCase;
 
@@ -39,17 +43,25 @@ class TemporalTraitTest extends KernelTestCase
         /** @var TemporalCreatedTrait $mock */
         $mock = $this->getMockForTrait('Sindla\Bundle\AuroraBundle\Entity\Super\TemporalCreatedTrait');
 
-
-        //$someDateTime = new \DateTime('2021-01-12 01:02:03');
-
         $someDateTime = new \DateTimeImmutable('2021-01-12 01:02:03');
         $dateFormat   = 'Y-m-d H:i:s';
         $mock->setCreatedAt($someDateTime);
-
         $this->assertTrue($someDateTime->format($dateFormat) == $mock->getCreatedAt()->format($dateFormat));
 
 //        $mock->expects($this->any())
 //            ->method('setCreatedAt')
 //            ->will($this->returnValue(TRUE));
+    }
+
+    public function testTemporalCreatedTraitException()
+    {
+        /** @var TemporalCreatedTrait $mock */
+        $mock = $this->getMockForTrait('Sindla\Bundle\AuroraBundle\Entity\Super\TemporalCreatedTrait');
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $someDateTime = new DateTime('2021-01-12 01:02:03');
+        $mock->setCreatedAt(new DateTime());
+        $mock->getCreatedAt();
     }
 }
