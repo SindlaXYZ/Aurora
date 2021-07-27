b259d0c8d8d3eec6cedb93560d26f523b0e8f943
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2021-07-27 13:55:42 +0300

    + Ownable trait & OwnableSubscriber

---
+++ src/Entity/Super/Ownable.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Entity\Super;
+
+// Symfony
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+trait Ownable
+{
+    /**
+     * @var UserInterface|null
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", cascade={"persist"})
+     * @ORM\JoinColumn(name="owner_id", referencedColumnName="id", nullable=true)
+     */
+    protected ?UserInterface $owner = null;
+
+    /**
+     * @var UserInterface|null
+     *
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", cascade={"persist"})
+     * @ORM\JoinColumn(name="created_by", referencedColumnName="id", nullable=true)
+     */
+    protected ?UserInterface $createdBy = null;
+
+    /**
+     * @var UserInterface|null
+     *
+     * @ORM\ManyToOne(targetEntity="App\Auth\Entity\User", cascade={"persist"})
+     * @ORM\JoinColumn(name="updated_by", referencedColumnName="id", nullable=true)
+     */
+    protected ?UserInterface $updatedBy = null;
+
+    /**
+     * @return UserInterface|null
+     */
+    public function getOwner(): ?UserInterface
+    {
+        return $this->owner;
+    }
+
+    /**
+     * @param UserInterface|null $owner
+     *
+     * @return $this
+     */
+    public function setOwner(?UserInterface $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return UserInterface|null
+     */
+    public function getCreatedBy(): ?UserInterface
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param UserInterface $createdBy
+     *
+     * @return $this
+     */
+    public function setCreatedBy(UserInterface $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * @return UserInterface|null
+     */
+    public function getUpdatedBy(): ?UserInterface
+    {
+        return $this->updatedBy;
+    }
+
+    /**
+     * @param UserInterface $updatedBy
+     *
+     * @return $this
+     */
+    public function setUpdatedBy(UserInterface $updatedBy): self
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+}
---
+++ src/EventSubscriber/OwnableSubscriber.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sindla\Bundle\AuroraBundle\EventSubscriber;
+
+// Symfony
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+// Aurora
+use Sindla\Bundle\AuroraBundle\Entity\Super;
+
+/**
+ * Updates owner_id, created_by and updated_by when creating or updating a resource, only if the resource uses Ownable trait
+ *
+ * services.yaml:
+ *
+    Sindla\Bundle\AuroraBundle\EventSubscriber\OwnableSubscriber:
+        arguments: [ "@security.token_storage" ]
+        tags:
+            - { name: doctrine.event_listener, event: prePersist, connection: default }
+            - { name: doctrine.event_listener, event: preUpdate, connection: default }
+ */
+class OwnableSubscriber implements EventSubscriberInterface
+{
+    private TokenStorageInterface $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::prePersist,
+            Events::preUpdate
+        ];
+    }
+
+    /**
+     * Before Create
+     *
+     * @param LifecycleEventArgs $args
+     */
+    public function prePersist(LifecycleEventArgs $args): void
+    {
+        /** @var Ownable $entity */
+        $entity = $args->getObject();
+
+        if (method_exists($entity, 'setOwner')) {
+            $entity->setOwner($this->getUser());
+        }
+
+        if (method_exists($entity, 'setCreatedBy')) {
+            $entity->setCreatedBy($this->getUser());
+        }
+
+        if (method_exists($entity, 'setUpdatedBy')) {
+            $entity->setUpdatedBy($this->getUser());
+        }
+    }
+
+    /**
+     * Before Update
+     *
+     * @param LifecycleEventArgs $args
+     */
+    public function preUpdate(LifecycleEventArgs $args): void
+    {
+        /** @var Ownable $entity */
+        $entity = $args->getObject();
+
+        if (method_exists($entity, 'setUpdatedBy')) {
+            $entity->setUpdatedBy($this->getUser());
+        }
+    }
+
+    /**
+     * @return UserInterface|User|null
+     */
+    private function getUser(): ?User
+    {
+        if (!$token = $this->tokenStorage->getToken()) {
+            return null;
+        }
+
+        if (!$token->isAuthenticated()) {
+            return null;
+        }
+
+        if (!$user = $token->getUser()) {
+            return null;
+        }
+
+        return $user;
+    }
+}
