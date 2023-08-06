4a289ce539075863c5c022b78f86031adada6b48
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2023-08-06 22:46:27 +0300

    ^

--- src/Doctrine/Attributes/Aurora.php
+++ src/Doctrine/Attributes/Aurora.php
@@ -4,11 +4,24 @@ namespace Sindla\Bundle\AuroraBundle\Doctrine\Attributes;
 
 use Attribute;
 
-#[Attribute]
+#[Attribute(Attribute::TARGET_PROPERTY)]
 class Aurora
 {
     public bool   $toSting      = false; // eg: #[Aurora(toSting: true)]
     public bool   $bitwise      = false; // eg: #[Aurora(bitwise: true)]
     public string $bitwiseConst = '';    // eg: #[Aurora(bitwiseConst: "STATUS_")]
     public bool   $json         = false; // eg: #[Aurora(json: true)]
+
+    public function __construct(
+        bool   $toSting,
+        bool   $bitwise,
+        string $bitwiseConst,
+        bool   $json
+    )
+    {
+        $this->toSting      = $toSting;
+        $this->bitwise      = $bitwise;
+        $this->bitwiseConst = $bitwiseConst;
+        $this->json         = $json;
+    }
 }
