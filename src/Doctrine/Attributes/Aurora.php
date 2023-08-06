45fd0a8dcf691502491ea12ff7edb851bfc36d85
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2023-08-06 22:49:38 +0300

    ^

--- src/Doctrine/Attributes/Aurora.php
+++ src/Doctrine/Attributes/Aurora.php
@@ -7,16 +7,16 @@ use Attribute;
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class Aurora
 {
-    public bool   $toSting      = false; // eg: #[Aurora(toSting: true)]
-    public bool   $bitwise      = false; // eg: #[Aurora(bitwise: true)]
-    public string $bitwiseConst = '';    // eg: #[Aurora(bitwiseConst: "STATUS_")]
-    public bool   $json         = false; // eg: #[Aurora(json: true)]
+    public bool    $toSting      = false;   // eg: #[Aurora(toSting: true)]
+    public bool    $bitwise      = false;   // eg: #[Aurora(bitwise: true)]
+    public ?string $bitwiseConst = null;    // eg: #[Aurora(bitwiseConst: "STATUS_")]
+    public bool    $json         = false;   // eg: #[Aurora(json: true)]
 
     public function __construct(
-        bool   $toSting,
-        bool   $bitwise,
-        string $bitwiseConst,
-        bool   $json
+        bool   $toSting = false,
+        bool   $bitwise = false,
+        string $bitwiseConst = null,
+        bool   $json = false
     )
     {
         $this->toSting      = $toSting;
