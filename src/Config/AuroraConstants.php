5f7a6af50d26ad9229863f03dcf4b08e804cbb66
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2024-02-07 09:10:28 +0200

    ^ composer: require php >=8.3 | + AuroraConstants | + Groups attribute to Identifiable & Timestampable entity traints

---
+++ src/Config/AuroraConstants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Config;
+
+class AuroraConstants
+{
+    public const string GROUP_READ  = 'Aurora:Read';
+    public const string GROUP_WRITE = 'Aurora:Write';
+}
