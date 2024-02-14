eb32a45c0832f495e44f15180e35d97d48e69f8b
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2024-02-14 20:48:16 +0200

    + Doctrine extension: CastAsVarchar

---
+++ src/Doctrine/DQL/PostgreSQL/CastAsVarchar.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Doctrine\DQL\PostgreSQL;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * Usage : StringFunction CAST(string)
+ */
+class CastAsVarchar extends FunctionNode
+{
+    private $string;
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return 'CAST(' . $this->string->dispatch($sqlWalker) . " AS VARCHAR)";
+    }
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->string = $parser->StringPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
