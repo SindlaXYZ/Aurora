73361112efa8dc6ffa6e7c2c6f71a5629a4c6a2a
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2024-03-15 10:07:29 +0200

    ^ GitHub Workflows & tests

--- tests/FakeTest.php
+++ tests/FakeTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Sindla\Bundle\AuroraBundle\Tests;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 /**
  * clear; php phpunit.phar -c phpunit.xml.dist vendor/sindla/aurora/tests/FakeTest.php --no-coverage
@@ -13,14 +14,32 @@ class FakeTest extends TestCase
     ###################################################################################################################################################################################################
 
     /**
-     * @dataProvider dataFakeDataProvider
+     * @dataProvider dataFakeDataProviderAnnotation
      */
-    public function testFakeDataProvider($given, $expected): void
+    public function testFakeDataProviderAnnotation($given, $expected): void
     {
         $this->assertEquals($given, $expected);
     }
 
-    public static function dataFakeDataProvider(): array
+    public static function dataFakeDataProviderAnnotation(): array
+    {
+        return [
+            [1, 1],
+            ['1', "1"],
+            [1, '1'],
+            ['1', 1]
+        ];
+    }
+
+    ###################################################################################################################################################################################################
+
+    #[DataProvider('dataFakeDataProviderAttribute')]
+    public function testFakeDataProviderAttribute($given, $expected): void
+    {
+        $this->assertEquals($given, $expected);
+    }
+
+    public static function dataFakeDataProviderAttribute(): array
     {
         return [
             [1, 1],
