39399f1a003fb1d1100baef6bb719ade06a5c464
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2024-03-15 12:02:05 +0200

    ^ GitHub Workflows & tests

--- scripts/dev-test.sh
+++ scripts/dev-test.sh
@@ -2,9 +2,9 @@ echo -e "Aurora short version (short version - X.Y, eg: 5.2, 6.3, 7.0, etc):" ;\
 read auroraVersion ;\
 composer create-project symfony/skeleton:${auroraVersion}.x-dev --no-cache . ;\
 yes | composer require symfony/webapp-pack ;\
-yes | composer require symfony/phpunit-bridge:${auroraVersion}.* ;\
-yes | composer require phpunit/phpunit:^10.5 ;\
-yes | composer require sindla/aurora:${auroraVersion}.x-dev --no-progress ;\
+yes | composer require sindla/aurora:${auroraVersion}.x-dev --no-cache --no-progress ;\
+yes | composer require phpunit/phpunit:10.5.* -W --dev --no-progress ;\
+yes | composer require dama/doctrine-test-bundle:8.0.* -W --dev --no-progress ;\
 curl -o phpunit.xml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/SindlaXYZ/Aurora/${auroraVersion}/phpunit.xml ;\
 curl -o config/packages/aurora.yaml -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/SindlaXYZ/Aurora/${auroraVersion}/src/Resources/schema/packages/aurora.yaml ;\
 curl -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/SindlaXYZ/Aurora/${auroraVersion}/src/Resources/schema/routes.append.yaml >> config/routes.yaml ;\
@@ -18,5 +18,3 @@ echo -e "\n" ;\
 php -v ;\
 bin/phpunit --version ;\
 vendor/bin/phpunit --version
-
-
