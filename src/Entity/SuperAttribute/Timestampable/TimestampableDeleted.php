ab14c0aea332f9291a02e8bdf26fed796419ce8c
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2024-05-12 20:39:42 +0300

    ^ Entity traits

--- src/Entity/SuperAttribute/Timestampable/TimestampableDeleted.php
+++ src/Entity/SuperAttribute/Timestampable/TimestampableDeleted.php
@@ -2,7 +2,6 @@
 
 namespace Sindla\Bundle\AuroraBundle\Entity\SuperAttribute\Timestampable;
 
-use DateTimeInterface;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Sindla\Bundle\AuroraBundle\Config\AuroraConstants;
