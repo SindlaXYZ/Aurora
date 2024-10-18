25f587231836579f0d8885d92f6a70910634e564
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2024-10-18 09:49:17 +0300

    ^ BlackHoleSubscriber

--- src/EventSubscriber/BlackHoleSubscriber.php
+++ src/EventSubscriber/BlackHoleSubscriber.php
@@ -30,56 +30,58 @@ readonly class BlackHoleSubscriber implements EventSubscriberInterface
 
     public function onKernelException(ExceptionEvent $event): void
     {
-        $this->auroraClient->ip($event->getRequest());
-        if (
-            isset($_ENV['BLACK_HOLE_API_URL'])
-            && str_starts_with($_ENV['BLACK_HOLE_API_URL'], 'http')
-            && isset($_ENV['BLACK_HOLE_API_VERSION'])
-            && !empty($_ENV['BLACK_HOLE_API_VERSION'])
-            && isset($_ENV['BLACK_HOLE_API_ENDPOINT'])
-            && !empty($_ENV['BLACK_HOLE_API_ENDPOINT'])
-            && isset($_ENV['BLACK_HOLE_API_BEARER'])
-            && !empty($_ENV['BLACK_HOLE_API_BEARER'])
-        ) {
-            $exception = $event->getThrowable();
-            if ($exception instanceof NotFoundHttpException) {
-                $client = HttpClient::create();
-                try {
-                    $request = $event->getRequest();
-                    $payload = [
-                        'method'    => $request->getMethod(),
-                        'url'       => $event->getRequest()->getUri(),
-                        'scheme'    => $request->getScheme(),
-                        'host'      => $request->getHost(),
-                        'port'      => $request->getPort() ?? 80,
-                        'path'      => $request->getPathInfo(),
-                        'query'     => $request->getQueryString(),
-                        'isHTTP'    => intval(!$request->isSecure()),
-                        'isHTTPS'   => intval($request->isSecure()),
-                        'serverIP'  => $request->server->get('SERVER_ADDR'),
-                        'clientIP'  => $this->auroraClient->ip($event->getRequest()),
-                        'userAgent' => $request->headers->get('User-Agent'),
-                        'headers'   => $request->headers->all(),
-                    ];
+        $request = $event->getRequest();
+        if ($event->isMainRequest()) {
+            $this->auroraClient->ip($request);
+            if (
+                isset($_ENV['BLACK_HOLE_API_URL'])
+                && str_starts_with($_ENV['BLACK_HOLE_API_URL'], 'http')
+                && isset($_ENV['BLACK_HOLE_API_VERSION'])
+                && !empty($_ENV['BLACK_HOLE_API_VERSION'])
+                && isset($_ENV['BLACK_HOLE_API_ENDPOINT'])
+                && !empty($_ENV['BLACK_HOLE_API_ENDPOINT'])
+                && isset($_ENV['BLACK_HOLE_API_BEARER'])
+                && !empty($_ENV['BLACK_HOLE_API_BEARER'])
+            ) {
+                $exception = $event->getThrowable();
+                if ($exception instanceof NotFoundHttpException) {
+                    $client = HttpClient::create();
+                    try {
+                        $payload = [
+                            'method'    => $request->getMethod(),
+                            'url'       => $event->getRequest()->getUri(),
+                            'scheme'    => $request->getScheme(),
+                            'host'      => $request->getHost(),
+                            'port'      => $request->getPort() ?? 80,
+                            'path'      => $request->getPathInfo(),
+                            'query'     => $request->getQueryString(),
+                            'isHTTP'    => intval(!$request->isSecure()),
+                            'isHTTPS'   => intval($request->isSecure()),
+                            'serverIP'  => $request->server->get('SERVER_ADDR'),
+                            'clientIP'  => $this->auroraClient->ip($event->getRequest()),
+                            'userAgent' => $request->headers->get('User-Agent'),
+                            'headers'   => $request->headers->all(),
+                        ];
 
-                    $client->request(
-                        'POST',
-                        (new Strink())->string(sprintf(
-                            '%s/%s/%s',
-                            $_ENV['BLACK_HOLE_API_URL'],
-                            $_ENV['BLACK_HOLE_API_VERSION'],
-                            $_ENV['BLACK_HOLE_API_ENDPOINT']
-                        ))->compressSlashes()->__toString(),
-                        [
-                            'headers' => [
-                                'Content-Type'  => 'application/json',
-                                'Authorization' => 'Bearer ' . $_ENV['BLACK_HOLE_API_VERSION']
-                            ],
-                            'json'    => $payload,
-                        ]
-                    );
-                } catch (\Exception|TransportExceptionInterface $e) {
-                    // do nothing
+                        $client->request(
+                            'POST',
+                            (new Strink())->string(sprintf(
+                                '%s/%s/%s',
+                                $_ENV['BLACK_HOLE_API_URL'],
+                                $_ENV['BLACK_HOLE_API_VERSION'],
+                                $_ENV['BLACK_HOLE_API_ENDPOINT']
+                            ))->compressSlashes()->__toString(),
+                            [
+                                'headers' => [
+                                    'Content-Type'  => 'application/json',
+                                    'Authorization' => 'Bearer ' . $_ENV['BLACK_HOLE_API_VERSION']
+                                ],
+                                'json'    => $payload,
+                            ]
+                        );
+                    } catch (\Exception|TransportExceptionInterface $e) {
+                        // do nothing
+                    }
                 }
             }
         }
