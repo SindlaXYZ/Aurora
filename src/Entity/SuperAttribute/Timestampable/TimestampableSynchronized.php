5c04bc4dc425dd3a7f4263332ce91022fbedf5bb
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2024-10-20 10:48:59 +0300

    + Entity trait: TimestampableSynchronized

---
+++ src/Entity/SuperAttribute/Timestampable/TimestampableSynchronized.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Entity\SuperAttribute\Timestampable;
+
+use DateTimeInterface;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Sindla\Bundle\AuroraBundle\Config\AuroraConstants;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+trait TimestampableSynchronized
+{
+    #[ORM\Column(name: 'synchronized_at', type: Types::DATETIME_IMMUTABLE, nullable: true)]
+    #[Groups([AuroraConstants::GROUP_READ])]
+    protected ?DateTimeInterface $synchronizedAt = null;
+
+    public function getSynchronizedAt(): ?DateTimeInterface
+    {
+        return $this->synchronizedAt;
+    }
+
+    public function setSynchronizedAt(?DateTimeInterface $synchronizedAt): self
+    {
+        $this->synchronizedAt = $synchronizedAt;
+        return $this;
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------------------------------------
+    // -- CUSTOM METHODS ----------------------------------------------------------------------------------------------------------------------------
+
+    public function isSynchronized(): bool
+    {
+        return boolval($this->synchronizedAt);
+    }
+
+    #[Groups([AuroraConstants::GROUP_READ])]
+    public function getSynchronizedAtLifespanAsSeconds(): int
+    {
+        return $this->synchronizedAt ? (new \DateTime())->getTimestamp() - $this->synchronizedAt->getTimestamp() : 0;
+    }
+
+    #[Groups([AuroraConstants::GROUP_READ])]
+    public function getSynchronizedAtLifespanAsMinutes(): int
+    {
+        return round($this->getSynchronizedAtLifespanAsSeconds() / 60);
+    }
+
+    #[Groups([AuroraConstants::GROUP_READ])]
+    public function getSynchronizedAtLifespanAsHours(): int
+    {
+        return round($this->getSynchronizedAtLifespanAsMinutes() / 60);
+    }
+
+    #[Groups([AuroraConstants::GROUP_READ])]
+    public function getSynchronizedAtLifespanAsDays(): int
+    {
+        return round($this->getSynchronizedAtLifespanAsHours() / 24);
+    }
+}
