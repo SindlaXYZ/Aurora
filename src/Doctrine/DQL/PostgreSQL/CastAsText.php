4c94dcabe9b02d38ce833fd22a372112885ca2ec
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2024-12-30 15:42:39 +0200

    ^ DQL/PostgreSQL/CastAsText

--- src/Doctrine/DQL/PostgreSQL/CastAsText.php
+++ src/Doctrine/DQL/PostgreSQL/CastAsText.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
 
 /**
  * CASTASTEXT: Sindla\Bundle\AuroraBundle\Doctrine\DQL\PostgreSQL\CastAsText
@@ -18,18 +19,24 @@ class CastAsText extends FunctionNode
 {
     private $string;
 
-    public function getSql(SqlWalker $sqlWalker)
+    public function getSql(SqlWalker $sqlWalker): string
     {
+        return sprintf(
+            'CAST(%s AS TEXT)',
+            $sqlWalker->walkSimpleArithmeticExpression($this->string)
+        );
+
+
         return 'CAST(' . $this->string->dispatch($sqlWalker) . " AS TEXT)";
     }
 
-    public function parse(Parser $parser)
+    public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
 
-        $this->string = $parser->StringPrimary();
+        $this->string = $parser->SimpleArithmeticExpression();
 
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 }
