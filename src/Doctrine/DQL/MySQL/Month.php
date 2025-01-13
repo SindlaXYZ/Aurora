bd15280ac7d19abbe78f015240ff996599eb68b3
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-01-13 10:29:29 +0200

    - EventSubscriber/FixDefaultSchemaSubscriber / ^ MySQL functions

--- src/Doctrine/DQL/MySQL/Month.php
+++ src/Doctrine/DQL/MySQL/Month.php
@@ -3,9 +3,9 @@
 namespace Sindla\Bundle\AuroraBundle\Doctrine\DQL\MySQL;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
 
 class Month extends FunctionNode
 {
@@ -24,11 +24,9 @@ class Month extends FunctionNode
 
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
-
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
         $this->date = $parser->ArithmeticPrimary();
-
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 }
