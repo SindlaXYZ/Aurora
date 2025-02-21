1270f75aa5dd1b85ba2c3afb1f3d6c46e1ab28f3
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-02-21 09:54:57 +0200

    + IdentifiableBigintNonNullable trait

---
+++ src/Entity/SuperAttribute/Identifiable/IdentifiableBigintNonNullable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Entity\SuperAttribute\Identifiable;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Sindla\Bundle\AuroraBundle\Config\AuroraConstants;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+trait IdentifiableBigintNonNullable
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'SEQUENCE')]
+    #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['unsigned' => true])]
+    #[Groups([AuroraConstants::GROUP_READ])]
+    protected int $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+}
