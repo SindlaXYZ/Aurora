1feee7a9a3721e06bea01d7dff82ac9bae646265
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-03-20 17:49:12 +0200

    + AuroraCalendarLinkGenerator & PHPUnit tests

---
+++ src/Utils/AuroraCalendarLinkGenerator/AuroraCalendarLinkGenerator.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Utils\AuroraCalendarLinkGenerator;
+
+class AuroraCalendarLinkGenerator
+{
+    public function __construct(
+        protected string             $title,
+        protected \DateTimeInterface $start,
+        protected \DateTimeInterface $end,
+        protected string             $description = '',
+        protected string             $location = ''
+    )
+    {
+    }
+
+    private function formatDateForGoogle(\DateTimeInterface $date): string
+    {
+        return $date->format('Ymd\THis');
+    }
+
+    private function formatDateForOutlook(\DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d\TH:i:s');
+    }
+
+    public function getGoogleCalendarLink(): string
+    {
+        $startFormatted = $this->formatDateForGoogle($this->start);
+        $endFormatted   = $this->formatDateForGoogle($this->end);
+
+        $params = [
+            'action'   => 'TEMPLATE',
+            'text'     => $this->title,
+            'dates'    => $startFormatted . '/' . $endFormatted,
+            'details'  => $this->description,
+            'location' => $this->location
+        ];
+
+        return 'https://calendar.google.com/calendar/render?' . http_build_query($params);
+    }
+
+    public function getYahooCalendarLink(): string
+    {
+        $startFormatted = $this->formatDateForGoogle($this->start);
+        $endFormatted   = $this->formatDateForGoogle($this->end);
+
+        $params = [
+            'v'      => 60,
+            'title'  => $this->title,
+            'st'     => $startFormatted,
+            'et'     => $endFormatted,
+            'desc'   => $this->description,
+            'in_loc' => $this->location
+        ];
+
+        return 'https://calendar.yahoo.com/?' . http_build_query($params);
+    }
+
+    public function getWebOutlookLink(): string
+    {
+        $startFormatted = $this->formatDateForOutlook($this->start);
+        $endFormatted   = $this->formatDateForOutlook($this->end);
+
+        $params = [
+            'path'     => '/calendar/action/compose',
+            'rru'      => 'addevent',
+            'subject'  => $this->title,
+            'startdt'  => $startFormatted,
+            'enddt'    => $endFormatted,
+            'body'     => $this->description,
+            'location' => $this->location
+        ];
+
+        return 'https://outlook.live.com/owa/?' . http_build_query($params);
+    }
+
+    public function getWebOfficeLink(): string
+    {
+        $startFormatted = $this->formatDateForOutlook($this->start);
+        $endFormatted   = $this->formatDateForOutlook($this->end);
+
+        $params = [
+            'subject'  => $this->title,
+            'body'     => $this->description,
+            'startdt'  => $startFormatted,
+            'enddt'    => $endFormatted,
+            'location' => $this->location
+        ];
+
+        return 'https://outlook.office.com/calendar/0/deeplink/compose?' . http_build_query($params);
+    }
+}
---
+++ tests/Utils/AuroraCalendarLinkGenerator/AuroraCalendarLinkGeneratorTest.php
@@ -0,0 +1,125 @@
+<?php
+declare(strict_types=1);
+
+namespace Sindla\Bundle\AuroraBundle\Tests\Utils\AuroraCalendar;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use Sindla\Bundle\AuroraBundle\Utils\AuroraCalendarLinkGenerator\AuroraCalendarLinkGenerator;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * clear; php phpunit.phar -c phpunit.xml.dist vendor/sindla/aurora/tests/Utils/AuroraCalendarLinkGenerator/AuroraCalendarLinkGeneratorTest.php --no-coverage
+ */
+class AuroraCalendarLinkGeneratorTest extends KernelTestCase
+{
+    private $kernelTest;
+
+    protected function setUp(): void
+    {
+        $this->kernelTest = self::bootKernel();
+    }
+
+    ###################################################################################################################################################################################################
+
+    public function testGoogleCalendarLinkWithDateTime(): void
+    {
+        $title       = 'Test Event';
+        $start       = new \DateTime('2025-04-15 10:00:00', new \DateTimeZone('UTC'));
+        $end         = new \DateTime('2025-04-15 11:00:00', new \DateTimeZone('UTC'));
+        $description = 'Event Description';
+        $location    = 'Test Location';
+
+        $generator = new AuroraCalendarLinkGenerator($title, $start, $end, $description, $location);
+        $link      = $generator->getGoogleCalendarLink();
+
+        // Formatul pentru Google Calendar: Ymd\THis
+        $expectedStart = $start->format('Ymd\THis');
+        $expectedEnd   = $end->format('Ymd\THis');
+
+        $this->assertStringContainsString('https://calendar.google.com/calendar/render?', $link);
+        $this->assertStringContainsString("action=TEMPLATE", $link);
+        $this->assertStringContainsString("text=" . urlencode($title), $link);
+        $this->assertStringContainsString("dates=" . urlencode($expectedStart . '/' . $expectedEnd), $link);
+        $this->assertStringContainsString("details=" . urlencode($description), $link);
+        $this->assertStringContainsString("location=" . urlencode($location), $link);
+    }
+
+    ###################################################################################################################################################################################################
+
+    public function testYahooCalendarLinkWithDateTimeImmutable(): void
+    {
+        $title       = 'Test Event';
+        $start       = new \DateTimeImmutable('2025-04-15 10:00:00', new \DateTimeZone('UTC'));
+        $end         = new \DateTimeImmutable('2025-04-15 11:00:00', new \DateTimeZone('UTC'));
+        $description = 'Event Description';
+        $location    = 'Test Location';
+
+        $generator = new AuroraCalendarLinkGenerator($title, $start, $end, $description, $location);
+        $link      = $generator->getYahooCalendarLink();
+
+        // Yahoo folosește același format ca Google: Ymd\THis
+        $expectedStart = $start->format('Ymd\THis');
+        $expectedEnd   = $end->format('Ymd\THis');
+
+        $this->assertStringContainsString('https://calendar.yahoo.com/?', $link);
+        $this->assertStringContainsString("v=60", $link);
+        $this->assertStringContainsString("title=" . urlencode($title), $link);
+        $this->assertStringContainsString("st=" . urlencode($expectedStart), $link);
+        $this->assertStringContainsString("et=" . urlencode($expectedEnd), $link);
+        $this->assertStringContainsString("desc=" . urlencode($description), $link);
+        $this->assertStringContainsString("in_loc=" . urlencode($location), $link);
+    }
+
+    ###################################################################################################################################################################################################
+
+    public function testWebOutlookLink(): void
+    {
+        $title       = 'Test Event';
+        $start       = new \DateTime('2025-04-15 10:00:00', new \DateTimeZone('UTC'));
+        $end         = new \DateTime('2025-04-15 11:00:00', new \DateTimeZone('UTC'));
+        $description = 'Event Description';
+        $location    = 'Test Location';
+
+        $generator = new AuroraCalendarLinkGenerator($title, $start, $end, $description, $location);
+        $link      = $generator->getWebOutlookLink();
+
+        // Formatul pentru Outlook: Y-m-d\TH:i:s
+        $expectedStart = $start->format('Y-m-d\TH:i:s');
+        $expectedEnd   = $end->format('Y-m-d\TH:i:s');
+
+        $this->assertStringContainsString('https://outlook.live.com/owa/?', $link);
+        $this->assertStringContainsString("rru=addevent", $link);
+        $this->assertStringContainsString("subject=" . urlencode($title), $link);
+        $this->assertStringContainsString("startdt=" . urlencode($expectedStart), $link);
+        $this->assertStringContainsString("enddt=" . urlencode($expectedEnd), $link);
+        $this->assertStringContainsString("body=" . urlencode($description), $link);
+        $this->assertStringContainsString("location=" . urlencode($location), $link);
+    }
+
+    ###################################################################################################################################################################################################
+
+    public function testWebOfficeLinkWithDateTimeImmutable(): void
+    {
+        $title       = 'Test Event';
+        $start       = new \DateTimeImmutable('2025-04-15 10:00:00', new \DateTimeZone('UTC'));
+        $end         = new \DateTimeImmutable('2025-04-15 11:00:00', new \DateTimeZone('UTC'));
+        $description = 'Event Description';
+        $location    = 'Test Location';
+
+        $generator = new AuroraCalendarLinkGenerator($title, $start, $end, $description, $location);
+        $link      = $generator->getWebOfficeLink();
+
+        // Formatul pentru Office: Y-m-d\TH:i:s
+        $expectedStart = $start->format('Y-m-d\TH:i:s');
+        $expectedEnd   = $end->format('Y-m-d\TH:i:s');
+
+        $this->assertStringContainsString('https://outlook.office.com/calendar/0/deeplink/compose?', $link);
+        $this->assertStringContainsString("subject=" . urlencode($title), $link);
+        $this->assertStringContainsString("startdt=" . urlencode($expectedStart), $link);
+        $this->assertStringContainsString("enddt=" . urlencode($expectedEnd), $link);
+        $this->assertStringContainsString("body=" . urlencode($description), $link);
+        $this->assertStringContainsString("location=" . urlencode($location), $link);
+    }
+
+    ###################################################################################################################################################################################################
+}
