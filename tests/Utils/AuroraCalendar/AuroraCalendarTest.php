58c8059f0968fd0f058c48c4ef7b978beba52790
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-04-11 21:26:25 +0300

    ^ AuroraCalendar::generateCalendar + PHPUnit tests

--- tests/Utils/AuroraCalendar/AuroraCalendarTest.php
+++ tests/Utils/AuroraCalendar/AuroraCalendarTest.php
@@ -139,7 +139,7 @@ class AuroraCalendarTest extends KernelTestCase
                 ]
             ],
             [
-                new AuroraCalendar()->generateCalendar(new \DateTimeImmutable('2025-04-11'), 1, 1, 0),
+                new AuroraCalendar()->generateCalendar(new \DateTimeImmutable('2025-04-11'), 7, 1, 0),
                 [
                     'days' => [
                         '2025-03-30', '2025-03-31', '2025-04-01', '2025-04-02', '2025-04-03', '2025-04-04', '2025-04-05',
@@ -148,12 +148,12 @@ class AuroraCalendarTest extends KernelTestCase
                 ]
             ],
             [
-                new AuroraCalendar()->generateCalendar(new \DateTimeImmutable('2025-04-11'), 1, 1, 1),
+                new AuroraCalendar()->generateCalendar(new \DateTimeImmutable('2025-04-11'), 7, 1, 1),
                 [
                     'days' => [
                         '2025-03-30', '2025-03-31', '2025-04-01', '2025-04-02', '2025-04-03', '2025-04-04', '2025-04-05',
                         '2025-04-06', '2025-04-07', '2025-04-08', '2025-04-09', '2025-04-10', '2025-04-11', '2025-04-12',
-                        '2025-04-13', '2025-04-14', '2025-04-15', '2025-04-16', '2025-04-17', '2025-04-18', '2025-04-19',
+                        '2025-04-13', '2025-04-14', '2025-04-15', '2025-04-16', '2025-04-17', '2025-04-18', '2025-04-19'
                     ],
                 ]
             ],
