f1b2111ffdd05b494ef7d16a98bf7e9847dc4d8f
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-06-24 12:11:23 +0300

    ^ tests

---
+++ tests/Entity/SuperAttribute/Identifiable/IdentifiableIntNonNullableTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Sindla\Bundle\AuroraBundle\Tests\Entity\SuperAnnotation;
+
+use PHPUnit\Framework\TestCase;
+use Sindla\Bundle\AuroraBundle\Entity\SuperAttribute\Identifiable\IdentifiableIntNonNullable;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * clear; php phpunit.phar -c phpunit.xml.dist vendor/sindla/aurora/tests/Entity/SuperAttribute/Identifiable/IdentifiableIntNonNullable.php --no-coverage
+ */
+class IdentifiableIntNonNullableTest extends KernelTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @group aurora
+     */
+    public function testIdentifiableUUID(): void
+    {
+        $Mock = new Mock();
+        $Mock->generateId();
+        $this->assertIsString($Mock->getId()->toBinary());
+    }
+}
+
+class Mock
+{
+    use IdentifiableIntNonNullable;
+}
