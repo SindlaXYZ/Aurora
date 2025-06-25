4331e07d4283d77d9246b299cd23dff7813c6210
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-06-25 09:14:47 +0300

    ^ Symfony 7 files & tests

---
+++ src/Entity/SuperAttribute/Identifiable/IdentifiableIntNonNullableStrategyCustom.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Sindla\Bundle\AuroraBundle\Entity\SuperAttribute\Identifiable;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Sindla\Bundle\AuroraBundle\Config\AuroraConstants;
+use Symfony\Component\Uid\Uuid;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+trait IdentifiableIntNonNullableStrategyCustom
+{
+    #[ORM\Id]
+    #[ORM\Column(type: Types::INTEGER, nullable: false, options: ['unsigned' => true])]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[Groups([AuroraConstants::GROUP_READ])]
+    protected int $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function generateId(): self
+    {
+        $this->id = Uuid::v7();
+        return $this;
+    }
+}
