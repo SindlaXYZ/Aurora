e244c85c943605fbc4ce33c3f8bd119688d6f97a
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-06-27 16:23:14 +0300

    ^

--- tests/Entity/SuperAttribute/Timestampable/TimestampableCreatedTest.php
+++ tests/Entity/SuperAttribute/Timestampable/TimestampableCreatedTest.php
@@ -3,13 +3,8 @@ declare(strict_types=1);
 
 namespace Sindla\Bundle\AuroraBundle\Tests\Entity\SuperAttribute\Timestampable;
 
-use DateTime;
-use TypeError;
-use PHPUnit\Framework\TestCase;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\HttpClient\HttpClient;
 use Sindla\Bundle\AuroraBundle\Entity\SuperAttribute\Timestampable\TimestampableCreated;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
  * clear; php phpunit.phar -c phpunit.xml.dist vendor/sindla/aurora/tests/Entity/SuperAttribute/Timestampable/TimestampableCreated.php --no-coverage
@@ -36,12 +31,15 @@ class TimestampableCreatedTest extends KernelTestCase
         $datetime = new \DateTimeImmutable('2021-01-12 01:02:03');
 
         /** @var TimestampableCreated $timestampableCreated */
-        $timestampableCreated = $this->createMock(TimestampableCreatedMock::class)->setCreatedAt($datetime);
+        $timestampableCreated = $this->createMock(TimestampableCreatedMock::class);
+        $timestampableCreated->setCreatedAt($datetime);
 
-        $someDateTime = $datetime;
+        $someDateTime = clone $datetime;
         $dateFormat   = 'Y-m-d H:i:s';
-        $timestampableCreated->setCreatedAt($someDateTime);
-        $this->assertTrue($someDateTime->format($dateFormat) == $timestampableCreated->getCreatedAt()->format($dateFormat));
+        $this->assertEquals(
+            $someDateTime->format($dateFormat),
+            $timestampableCreated->getCreatedAt()->format($dateFormat)
+        );
     }
 
     public function testTimestampableCreatedException(): void
@@ -49,8 +47,8 @@ class TimestampableCreatedTest extends KernelTestCase
         /** @var TimestampableCreated $timestampableCreated */
         $timestampableCreated = $this->createMock(TimestampableCreatedMock::class);
 
-        $this->expectException(TypeError::class);
-        $timestampableCreated->setCreatedAt(new DateTime())->getCreatedAt();
+        $this->expectException(\TypeError::class);
+        $timestampableCreated->setCreatedAt(new \DateTime())->getCreatedAt();
     }
 }
 
