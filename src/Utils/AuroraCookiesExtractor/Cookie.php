463252560a9629d0fb5e6245b0083d9ed0578bef
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-08-21 10:09:50 +0300

    ^ aurora.cookies.extractor

--- src/Utils/AuroraCookiesExtractor/Cookie.php
+++ src/Utils/AuroraCookiesExtractor/Cookie.php
@@ -4,17 +4,17 @@ namespace Sindla\Bundle\AuroraBundle\Utils\AuroraCookiesExtractor;
 
 class Cookie
 {
-    private string             $name;
-    private string             $value;
-    private string             $domain;
-    private string             $path;
-    private \DateTimeImmutable $expires;
-    private int                $size;
-    private bool               $httpOnly;
-    private bool               $secure;
-    private string             $sameSite;
-    private string             $priority;
-    private array              $attributes;
+    private ?string             $name       = null;
+    private ?string             $value      = null;
+    private ?string             $domain     = null;
+    private ?string             $path       = null;
+    private ?\DateTimeImmutable $expires    = null;
+    private ?int                $size       = null;
+    private ?bool               $httpOnly   = null;
+    private ?bool               $secure     = null;
+    private ?string             $sameSite   = null;
+    private ?string             $priority   = null;
+    private ?array              $attributes = null;
 
     public function toArray(): array
     {
@@ -26,145 +26,124 @@ class Cookie
         return $array;
     }
 
-    public function getName(): string
+    public function getName(): ?string
     {
         return $this->name;
     }
 
-    public function setName(string $name): self
+    public function setName(?string $name): self
     {
         $this->name = $name;
         return $this;
     }
 
-    public function getValue(): string
+    public function getValue(): ?string
     {
         return $this->value;
     }
 
-    public function setValue(string $value): self
+    public function setValue(?string $value): self
     {
         $this->value = $value;
         return $this;
     }
 
-    public function getDomain(): string
+    public function getDomain(): ?string
     {
         return $this->domain;
     }
 
-    public function setDomain(string $domain): self
+    public function setDomain(?string $domain): self
     {
         $this->domain = $domain;
         return $this;
     }
 
-    public function getPath(): string
+    public function getPath(): ?string
     {
         return $this->path;
     }
 
-    public function setPath(string $path): self
+    public function setPath(?string $path): self
     {
         $this->path = $path;
         return $this;
     }
 
-    public function getExpires(): \DateTimeImmutable
+    public function getExpires(): ?\DateTimeImmutable
     {
         return $this->expires;
     }
 
-    public function setExpires(\DateTimeImmutable $expires): self
+    public function setExpires(?\DateTimeImmutable $expires): self
     {
         $this->expires = $expires;
         return $this;
     }
 
-    public function getSize(): int
+    public function getSize(): ?int
     {
         return $this->size;
     }
 
-    public function setSize(int $size): self
+    public function setSize(?int $size): self
     {
         $this->size = $size;
         return $this;
     }
 
-    public function isHttpOnly(): bool
+    public function getHttpOnly(): ?bool
     {
         return $this->httpOnly;
     }
 
-    public function setHttpOnly(bool $httpOnly): self
+    public function setHttpOnly(?bool $httpOnly): self
     {
         $this->httpOnly = $httpOnly;
         return $this;
     }
 
-    public function isSecure(): bool
+    public function getSecure(): ?bool
     {
         return $this->secure;
     }
 
-    public function setSecure(bool $secure): self
+    public function setSecure(?bool $secure): self
     {
         $this->secure = $secure;
         return $this;
     }
 
-    public function getSameSite(): string
+    public function getSameSite(): ?string
     {
         return $this->sameSite;
     }
 
-    public function setSameSite(string $sameSite): self
+    public function setSameSite(?string $sameSite): self
     {
         $this->sameSite = $sameSite;
         return $this;
     }
 
-    public function getPriority(): string
+    public function getPriority(): ?string
     {
         return $this->priority;
     }
 
-    public function setPriority(string $priority): self
+    public function setPriority(?string $priority): self
     {
         $this->priority = $priority;
         return $this;
     }
 
-    public function getAttributes(): array
+    public function getAttributes(): ?array
     {
         return $this->attributes;
     }
 
-    public function setAttributes(array $attributes): self
+    public function setAttributes(?array $attributes): self
     {
         $this->attributes = $attributes;
-        return $this;
-    }
-
-    public function addAttribute(mixed $attribute): self
-    {
-        $this->attributes[] = $attribute;
-        return $this;
-    }
-
-    public function mergeAttributes(array $attributes): self
-    {
-        $this->attributes = (is_array($this->attributes) ? array_merge($this->attributes, $attributes) : $attributes);
-        return $this;
-    }
-
-    public function removeAttribute(mixed $attribute): self
-    {
-        if (true === in_array($attribute, $this->attributes, true)) {
-            $index = array_search($attribute, $this->attributes);
-            array_splice($this->attributes, $index, 1);
-        }
         return $this;
     }
 }
