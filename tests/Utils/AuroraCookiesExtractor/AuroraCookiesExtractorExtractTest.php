bc79307d2141e799191dd45ff16a3e5519c3a860
Sindla <47600629+SindlaXYZ@users.noreply.github.com>
2025-09-08 13:20:45 +0300

    Add test for max-age expiration

---
+++ tests/Utils/AuroraCookiesExtractor/AuroraCookiesExtractorExtractTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Symfony\Contracts\HttpClient {
+    interface ResponseInterface {
+        public function getInfo(?string $type = null): mixed;
+    }
+}
+
+namespace Sindla\Bundle\AuroraBundle\Tests\Utils\AuroraCookiesExtractor {
+    use DateTimeImmutable;
+    use DateTimeZone;
+    use PHPUnit\Framework\TestCase;
+    use Sindla\Bundle\AuroraBundle\Utils\AuroraCookiesExtractor\AuroraCookiesExtractor;
+    use Symfony\Contracts\HttpClient\ResponseInterface;
+
+    class AuroraCookiesExtractorExtractTest extends TestCase
+    {
+        public function testMaxAgeSetsRelativeExpiry(): void
+        {
+            $response = new class implements ResponseInterface {
+                public function getInfo(?string $type = null): mixed
+                {
+                    return [
+                        'response_headers' => ['set-cookie: session=abc; Max-Age=60']
+                    ];
+                }
+            };
+
+            $extractor = new AuroraCookiesExtractor();
+            $cookies = $extractor->extractFromSymfonyResponseInterface($response);
+
+            $this->assertCount(1, $cookies);
+            $expires = $cookies[0]->getExpires();
+            $this->assertInstanceOf(DateTimeImmutable::class, $expires);
+            $diff = $expires->getTimestamp() - (new DateTimeImmutable('now', new DateTimeZone('UTC')))->getTimestamp();
+            $this->assertGreaterThanOrEqual(59, $diff);
+            $this->assertLessThanOrEqual(60, $diff);
+        }
+    }
+}
