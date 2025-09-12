ebfed53b108ff0c0a85ef38c710e6eee5206a2bd
Sindla <47600629+SindlaXYZ@users.noreply.github.com>
2025-09-12 13:10:31 +0300

    Fix encryption output and add tests

---
+++ tests/Utils/AuroraCryptor/AuroraCryptorTest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Sindla\Bundle\AuroraBundle\Tests\Utils\AuroraCryptor;
+
+use PHPUnit\Framework\TestCase;
+use Sindla\Bundle\AuroraBundle\Utils\AuroraCryptor\AuroraCryptor;
+
+/**
+ * clear; php phpunit.phar -c phpunit.xml.dist vendor/sindla/aurora/tests/Utils/AuroraCryptor/AuroraCryptorTest.php --no-coverage
+ */
+class AuroraCryptorTest extends TestCase
+{
+    public function testEncryptAndDecrypt(): void
+    {
+        $cryptor = new AuroraCryptor();
+        $key = 'myPa$$worD123';
+        $data = 'megaSecretKey';
+
+        $encrypted = $cryptor->setEncryptionKey($key)->encrypt($data);
+        $decoded = base64_decode($encrypted, true);
+
+        $this->assertNotFalse($decoded);
+        $this->assertStringContainsString('::', $decoded);
+
+        $decrypted = $cryptor->setEncryptionKey($key)->decrypt($encrypted);
+        $this->assertSame($data, $decrypted);
+    }
+}
