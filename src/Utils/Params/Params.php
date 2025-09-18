9cd943ef24e305dda3a7168b2707ed17d5c42c5c
Sindla <47600629+SindlaXYZ@users.noreply.github.com>
2025-09-18 09:38:31 +0300

    Fix Params container type and add tests

--- src/Utils/Params/Params.php
+++ src/Utils/Params/Params.php
@@ -2,19 +2,42 @@
 
 namespace Sindla\Bundle\AuroraBundle\Utils\Params;
 
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\Yaml\Yaml;
 
 @trigger_error('Aurora/Params class is deprecated, use `config/packages/aurora.yaml` parameters: aurora.* instead.', E_USER_DEPRECATED);
 
 class Params
 {
-    private $container;
-    private $params;
+    private Container $container;
+
+    /**
+     * @var array<string, mixed>
+     */
+    private array $params;
 
     public function __construct(Container $Container)
     {
         $this->container = $Container;
-        $this->params    = Yaml::parse(file_get_contents($this->container->getParameter('kernel.project_dir') . '/config/packages/aurora.yaml'))['aurora'];
+
+        $configPath = $this->container->getParameter('kernel.project_dir') . '/config/packages/aurora.yaml';
+        $yaml       = file_get_contents($configPath);
+
+        if ($yaml === false) {
+            throw new \RuntimeException(sprintf('Unable to read configuration file "%s".', $configPath));
+        }
+
+        /** @var mixed $parsedRaw */
+        $parsedRaw = Yaml::parse($yaml);
+
+        if (!is_array($parsedRaw) || !isset($parsedRaw['aurora']) || !is_array($parsedRaw['aurora'])) {
+            throw new \UnexpectedValueException(sprintf('Configuration file "%s" must define an "aurora" array.', $configPath));
+        }
+
+        /** @var array<string, mixed> $parsed */
+        $parsed = $parsedRaw;
+
+        $this->params = $parsed['aurora'];
 
         if (isset($this->params['tmp'])) {
             $this->params['tmp'] = $this->container->getParameter('kernel.project_dir') . '/' . $this->params['tmp'];
@@ -30,7 +53,10 @@ class Params
 
     }
 
-    public function getAll()
+    /**
+     * @return array<string, mixed>
+     */
+    public function getAll(): array
     {
         return $this->params;
     }
