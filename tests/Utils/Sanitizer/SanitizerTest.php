8924d5347045a81cc206879c5584a62c57f5f676
Sindla <47600629+SindlaXYZ@users.noreply.github.com>
2025-09-18 06:55:16 +0300

    Document stub container behaviour

--- tests/Utils/Sanitizer/SanitizerTest.php
+++ tests/Utils/Sanitizer/SanitizerTest.php
@@ -4,11 +4,31 @@ namespace Symfony\Component\DependencyInjection {
     if (!interface_exists(ContainerInterface::class)) {
         interface ContainerInterface
         {
+            public const RUNTIME_EXCEPTION_ON_INVALID_REFERENCE = 0;
             public const EXCEPTION_ON_INVALID_REFERENCE = 1;
+            public const NULL_ON_INVALID_REFERENCE = 2;
+            public const IGNORE_ON_INVALID_REFERENCE = 3;
+            public const IGNORE_ON_UNINITIALIZED_REFERENCE = 4;
+
+            public function set(string $id, ?object $service): void;
 
             public function get(string $id, int $invalidBehavior = self::EXCEPTION_ON_INVALID_REFERENCE): ?object;
 
             public function has(string $id): bool;
+
+            public function initialized(string $id): bool;
+
+            /**
+             * @return array<int|string, mixed>|bool|float|int|string|\UnitEnum|null
+             */
+            public function getParameter(string $name): array|bool|string|int|float|\UnitEnum|null;
+
+            public function hasParameter(string $name): bool;
+
+            /**
+             * @param array<int|string, mixed>|bool|float|int|string|\UnitEnum|null $value
+             */
+            public function setParameter(string $name, array|bool|string|int|float|\UnitEnum|null $value): void;
         }
     }
 }
@@ -24,20 +44,43 @@ class SanitizerTest extends TestCase
     public function testHtmlMinifyUsesJsMinifier(): void
     {
         $container = new class implements ContainerInterface {
+            public function set(string $id, ?object $service): void {}
             public function get(string $id, int $invalidBehavior = ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE): ?object
             {
                 return null;
             }
             public function has(string $id): bool { return false; }
+            public function initialized(string $id): bool { return false; }
+            /**
+             * @return array<int|string, mixed>|bool|float|int|string|\UnitEnum|null
+             */
+            public function getParameter(string $name): array|bool|string|int|float|\UnitEnum|null { return null; }
+            public function hasParameter(string $name): bool { return false; }
+            /**
+             * @param array<int|string, mixed>|bool|float|int|string|\UnitEnum|null $value
+             */
+            public function setParameter(string $name, array|bool|string|int|float|\UnitEnum|null $value): void {}
         };
 
         $sanitizer = new class($container) extends Sanitizer {
+            /**
+             * @var list<string>
+             */
             public array $calls = [];
+            /**
+             * @param string $input
+             * @return string
+             */
             public function minifyCSS($input)
             {
                 $this->calls[] = 'css';
                 return $input;
             }
+            /**
+             * @param string $input
+             * @param bool   $removeConsoleOutputs
+             * @return string
+             */
             public function minifyJS($input, $removeConsoleOutputs = false)
             {
                 $this->calls[] = 'js';
