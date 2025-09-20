a942074cf92ce3fb3720c4057a7ef0b52a5fc26e
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-09-20 08:06:12 +0300

    ^ tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,18 +2,22 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-if(is_file(dirname(__DIR__).'/vendor/autoload.php')) {
+if (is_file(dirname(__DIR__) . '/vendor/autoload.php')) {
     require dirname(__DIR__) . '/vendor/autoload.php';
+} else if (is_file(dirname(__DIR__) . '/../../../autoload.php')) {
+    require dirname(__DIR__) . '/../../../autoload.php';
+} else if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
 } else {
-    require __DIR__.'/../vendor/autoload.php';
+    throw new \RuntimeException('Could not find the autoload.php file. Please run "composer install".');
 }
 
 if (!class_exists(\Symfony\Bundle\FrameworkBundle\Test\KernelTestCase::class)) {
     require __DIR__ . '/SymfonyKernelTestCaseStub.php';
 }
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
+    require dirname(__DIR__) . '/config/bootstrap.php';
+} else if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
