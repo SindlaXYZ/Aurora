78532f8d5be31a54253fcc8ede91fcebe327532d
Sindla <47600629+SindlaXYZ@users.noreply.github.com>
2025-09-21 12:05:00 +0300

    Handle microseconds in datetime format detection

--- src/Utils/AuroraChronos/AuroraChronos.php
+++ src/Utils/AuroraChronos/AuroraChronos.php
@@ -22,40 +22,40 @@ class AuroraChronos
             return 'Y-m-d H:i:s';
         }
 
-        if (preg_match('/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}$/', $datetime)) {
-            return 'Y-m-d H:i:s.v';
+        if (preg_match('/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.(\d{1,6})$/', $datetime, $matches)) {
+            return strlen($matches[1]) <= 3 ? 'Y-m-d H:i:s.v' : 'Y-m-d H:i:s.u';
         }
 
         if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/', $datetime)) {
             return 'Y-m-d\TH:i:s';
         }
 
-        if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}$/', $datetime)) {
-            return 'Y-m-d\TH:i:s.v';
+        if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.(\d{1,6})$/', $datetime, $matches)) {
+            return strlen($matches[1]) <= 3 ? 'Y-m-d\TH:i:s.v' : 'Y-m-d\TH:i:s.u';
         }
 
         if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/', $datetime)) {
             return 'Y-m-d\TH:i:s\Z';
         }
 
-        if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/', $datetime)) {
-            return 'Y-m-d\TH:i:s.v\Z';
+        if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.(\d{1,6})Z$/', $datetime, $matches)) {
+            return strlen($matches[1]) <= 3 ? 'Y-m-d\TH:i:s.v\Z' : 'Y-m-d\TH:i:s.u\Z';
         }
 
         if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+\-]\d{2}:\d{2}$/', $datetime)) {
             return 'Y-m-d\TH:i:sP';
         }
 
-        if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+\-]\d{2}:\d{2}$/', $datetime)) {
-            return 'Y-m-d\TH:i:s.vP';
+        if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.(\d{1,6})[+\-]\d{2}:\d{2}$/', $datetime, $matches)) {
+            return strlen($matches[1]) <= 3 ? 'Y-m-d\TH:i:s.vP' : 'Y-m-d\TH:i:s.uP';
         }
 
         if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+\-]\d{4}$/', $datetime)) {
             return 'Y-m-d\TH:i:sO';
         }
 
-        if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+\-]\d{4}$/', $datetime)) {
-            return 'Y-m-d\TH:i:s.vO';
+        if (preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.(\d{1,6})[+\-]\d{4}$/', $datetime, $matches)) {
+            return strlen($matches[1]) <= 3 ? 'Y-m-d\TH:i:s.vO' : 'Y-m-d\TH:i:s.uO';
         }
 
         if (preg_match('/^\d{2}\/\d{2}\/\d{4}$/', $datetime)) {
