f417afeb4312e3d6191ec5749a3ee5940a07c92a
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-09-21 09:48:56 +0300

    ^ install-7.3.sh

--- symfony/install-7.3.sh
+++ symfony/install-7.3.sh
@@ -1,17 +1,23 @@
-if [[ -d 7.3 ]]; then
-    echo "Directory 7.3 already exists."
-    cd 7.3
-    rm -rf .[!.]* ..?* *
-else
-    mkdir 7.3
-    cd 7.3
-fi
-yes | composer create-project symfony/skeleton:7.3.x-dev . --no-cache
-yes | composer require symfony/webapp-pack -W --no-progress
 parent=${PWD%/symfony*}
 win_parent=$(echo "$parent" | sed -E 's|^/([a-z])/(.*)|\u\1:/\2|')
-# read -p "sindla/aurora path (default $win_parent): " aurora_path
+win_parent_installed="${win_parent}-symfony"
+read -p "sindla/aurora path (default $win_parent): " aurora_path
 aurora_path=${aurora_path:-$win_parent}
+
+read -p "Symfony + sindla/aurora path (default $win_parent_installed): " aurora_installed_path
+aurora_installed_path=${aurora_installed_path:-$win_parent_installed}
+
+if [[ -d "$aurora_installed_path" ]]; then
+  echo "Directory "$aurora_installed_path" already exists."
+  cd "$aurora_installed_path"
+  rm -rf .[!.]* ..?* *
+else
+  mkdir "$aurora_installed_path"
+  cd "$aurora_installed_path"
+fi
+
+yes | composer create-project symfony/skeleton:7.3.x-dev . --no-cache
+yes | composer require symfony/webapp-pack -W --no-progress
 composer config repositories.aurora "{\"type\":\"path\",\"url\":\"$aurora_path\",\"options\":{\"symlink\":true}}"
 yes | composer require sindla/aurora:7.3.x-dev -W --no-progress
 yes | composer require phpunit/phpunit:12.3.* -W --dev --no-progress
