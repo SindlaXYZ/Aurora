d148ef29fedd5c6a276a8eb085faa039c519fb21
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-09-24 18:09:36 +0300

    ^ aurora:php-unit --action=updateDocumentationBlocks

--- tests/Utils/AuroraCookiesExtractor/AuroraCookiesExtractorExtractTest.php
+++ tests/Utils/AuroraCookiesExtractor/AuroraCookiesExtractorExtractTest.php
@@ -1,78 +1,78 @@
 <?php
 declare(strict_types=1);
 
-namespace Sindla\Bundle\AuroraBundle\Tests\Utils\AuroraCookiesExtractor {
-    use DateTimeImmutable;
-    use DateTimeZone;
-    use PHPUnit\Framework\TestCase;
-    use Sindla\Bundle\AuroraBundle\Utils\AuroraCookiesExtractor\AuroraCookiesExtractor;
-    use Symfony\Contracts\HttpClient\ResponseInterface;
+namespace Sindla\Bundle\AuroraBundle\Tests\Utils\AuroraCookiesExtractor;
 
-    class AuroraCookiesExtractorExtractTest extends TestCase
+use DateTimeImmutable;
+use DateTimeZone;
+use PHPUnit\Framework\TestCase;
+use Sindla\Bundle\AuroraBundle\Utils\AuroraCookiesExtractor\AuroraCookiesExtractor;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class AuroraCookiesExtractorExtractTest extends TestCase
+{
+    public function testMaxAgeSetsRelativeExpiry(): void
     {
-        public function testMaxAgeSetsRelativeExpiry(): void
-        {
-            $response = new class implements ResponseInterface {
-                public function getInfo(?string $type = null): mixed
-                {
-                    return [
-                        'response_headers' => ['set-cookie: session=abc; Max-Age=60']
-                    ];
-                }
-            };
+        $response = new class implements ResponseInterface {
+            public function getInfo(?string $type = null): mixed
+            {
+                return [
+                    'response_headers' => ['set-cookie: session=abc; Max-Age=60']
+                ];
+            }
+        };
 
-            $extractor = new AuroraCookiesExtractor();
-            $cookies = $extractor->extractFromSymfonyResponseInterface($response);
+        $extractor = new AuroraCookiesExtractor();
+        $cookies   = $extractor->extractFromSymfonyResponseInterface($response);
 
-            $this->assertCount(1, $cookies);
-            $expires = $cookies[0]->getExpires();
-            $this->assertInstanceOf(DateTimeImmutable::class, $expires);
-            $diff = $expires->getTimestamp() - (new DateTimeImmutable('now', new DateTimeZone('UTC')))->getTimestamp();
-            $this->assertGreaterThanOrEqual(59, $diff);
-            $this->assertLessThanOrEqual(60, $diff);
-        }
+        $this->assertCount(1, $cookies);
+        $expires = $cookies[0]->getExpires();
+        $this->assertInstanceOf(DateTimeImmutable::class, $expires);
+        $diff = $expires->getTimestamp() - (new DateTimeImmutable('now', new DateTimeZone('UTC')))->getTimestamp();
+        $this->assertGreaterThanOrEqual(59, $diff);
+        $this->assertLessThanOrEqual(60, $diff);
+    }
 
-        public function testHeaderCaseInsensitive(): void
-        {
-            $response = new class implements ResponseInterface {
-                public function getInfo(?string $type = null): mixed
-                {
-                    return [
-                        'response_headers' => ['Set-Cookie: test=1']
-                    ];
-                }
-            };
+    public function testHeaderCaseInsensitive(): void
+    {
+        $response = new class implements ResponseInterface {
+            public function getInfo(?string $type = null): mixed
+            {
+                return [
+                    'response_headers' => ['Set-Cookie: test=1']
+                ];
+            }
+        };
 
-            $extractor = new AuroraCookiesExtractor();
-            $cookies = $extractor->extractFromSymfonyResponseInterface($response);
+        $extractor = new AuroraCookiesExtractor();
+        $cookies   = $extractor->extractFromSymfonyResponseInterface($response);
 
-            $this->assertCount(1, $cookies);
-            $this->assertSame('test', $cookies[0]->getName());
-            $this->assertSame('1', $cookies[0]->getValue());
-        }
+        $this->assertCount(1, $cookies);
+        $this->assertSame('test', $cookies[0]->getName());
+        $this->assertSame('1', $cookies[0]->getValue());
+    }
 
-        public function testInvalidExpiresStringPreservesAttribute(): void
-        {
-            $response = new class implements ResponseInterface {
-                public function getInfo(?string $type = null): mixed
-                {
-                    return [
-                        'response_headers' => ['Set-Cookie: token=value; Expires=Not a valid date; Secure']
-                    ];
-                }
-            };
+    public function testInvalidExpiresStringPreservesAttribute(): void
+    {
+        $response = new class implements ResponseInterface {
+            public function getInfo(?string $type = null): mixed
+            {
+                return [
+                    'response_headers' => ['Set-Cookie: token=value; Expires=Not a valid date; Secure']
+                ];
+            }
+        };
 
-            $extractor = new AuroraCookiesExtractor();
-            $cookies = $extractor->extractFromSymfonyResponseInterface($response);
+        $extractor = new AuroraCookiesExtractor();
+        $cookies   = $extractor->extractFromSymfonyResponseInterface($response);
 
-            $this->assertCount(1, $cookies);
-            $cookie = $cookies[0];
+        $this->assertCount(1, $cookies);
+        $cookie = $cookies[0];
 
-            $this->assertNull($cookie->getExpires());
-            $this->assertSame(
-                ['Expires' => 'Not a valid date'],
-                $cookie->getAttributes()
-            );
-        }
+        $this->assertNull($cookie->getExpires());
+        $this->assertSame(
+            ['Expires' => 'Not a valid date'],
+            $cookie->getAttributes()
+        );
     }
 }
