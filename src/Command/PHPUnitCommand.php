d235d307b41cfa412b3088eb274c32fdb86f8007
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-10-17 11:14:04 +0300

    [WIP] Refactoring: CommandMiddleware

--- src/Command/PHPUnitCommand.php
+++ src/Command/PHPUnitCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Finder\Finder;
 
 #[AsCommand(
