120b37f5563eff33549f8ff68741f9ac8acf0bf3
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2025-10-24 18:10:15 +0300

    Bug fix: CommandMiddleware

--- src/Command/Middleware/CommandMiddleware.php
+++ src/Command/Middleware/CommandMiddleware.php
@@ -410,17 +410,17 @@ class CommandMiddleware extends Command
 
     protected function progressBarGetElapsedMinutes(): int|float
     {
-        return new \DateTimeImmutable()->getTimestamp() - $this->progressBar->getStartTime() / 60;
+        return $this->progressBarGetElapsedSeconds() / 60;
     }
 
     protected function progressBarGetElapsedHours(): int|float
     {
-        return new \DateTimeImmutable()->getTimestamp() - $this->progressBar->getStartTime() / 3600;
+        return $this->progressBarGetElapsedSeconds() / 3600;
     }
 
     protected function progressBarGetElapsedDays(): int|float
     {
-        return new \DateTimeImmutable()->getTimestamp() - $this->progressBar->getStartTime() / 86400;
+        return $this->progressBarGetElapsedSeconds() / 86400;
     }
 
     protected function progressBarComment(string $comment, int $step = 0): void
