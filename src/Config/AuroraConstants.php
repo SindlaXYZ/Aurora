b4582aaad755d34be8a80e5f3cbb622d2087cb8a
SindlaXYZ <liviu2019+SindlaXYZ+github.com@sindla.com>
2026-03-28 09:00:11 +0200

    ^

--- src/Config/AuroraConstants.php
+++ src/Config/AuroraConstants.php
@@ -4,9 +4,9 @@ namespace Sindla\Bundle\AuroraBundle\Config;
 
 class AuroraConstants
 {
-    public const string GROUP_READ                               = 'Aurora:Read';
-    public const string GROUP_READ_IDENTIFIABLE                  = 'Aurora:Read:Identifiable';
-    public const string GROUP_READ_TIMESTAMPABLE                 = 'Aurora:Read:Timestampable';
-    public const string GROUP_WRITE                              = 'Aurora:Write';
-    public const string TIMESTAMPABLE_DELETED_DEFAULT_DELETED_AT = '2222-02-22 22:22:22.222222';
+    final public const string GROUP_READ                               = 'Aurora:Read';
+    final public const string GROUP_READ_IDENTIFIABLE                  = 'Aurora:Read:Identifiable';
+    final public const string GROUP_READ_TIMESTAMPABLE                 = 'Aurora:Read:Timestampable';
+    final public const string GROUP_WRITE                              = 'Aurora:Write';
+    final public const string TIMESTAMPABLE_DELETED_DEFAULT_DELETED_AT = '2222-02-22 22:22:22.222222';
 }
